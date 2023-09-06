972c95440ed1462e61fc6ee39d67a3dc2b63ae28
El-Medonho <fredericoboller@outlook.com>
2023-09-06 18:35:40 -0300

    2023.09.06

---
+++ lista2/modifiedbfs.cpp
@@ -0,0 +1,52 @@
+#include "bits/stdc++.h"
+
+using namespace std;
+
+signed main(){
+    ios_base::sync_with_stdio(false), cin.tie(nullptr);
+
+    int n,m; cin >> n >> m;
+
+    vector<vector<int>> graph(n);
+
+    vector<int> pai(n,0), nivel(n,0);  //vou usar 0 indexado
+
+    while(m--){
+        int a,b; cin >> a >> b; a--; b--;
+        graph[a].push_back(b);
+        graph[b].push_back(a);
+    }
+
+    queue<int> qw;
+    vector<bool> vis(n,0);
+
+    qw.push(0);
+    vis[0] = 1;
+
+    while(!qw.empty()){
+        int cc = qw.front(); qw.pop();
+
+        for(int j: graph[cc]){
+            if(vis[j]) continue;
+            vis[j] = 1;
+            nivel[j] = nivel[cc]+1;
+            pai[j] = cc;
+            qw.push(j);
+        }
+    }
+
+    cout << "Nivel:\n";
+
+    int h = nivel.size();
+
+    for(int i = 0; i < h; i++) cout << nivel[i] << " \n"[i == h-1];
+
+    cout << "Pai:\n";
+
+    h = pai.size();
+
+    for(int i = 0; i < h; i++) cout << pai[i]+1 << " \n"[i == h-1];
+
+
+    return 0;
+}
